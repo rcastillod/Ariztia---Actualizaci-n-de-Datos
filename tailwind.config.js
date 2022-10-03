ca88b2d40d10eb3327b262f8e5287bd40a1f1c1a
rcastillod <rodrigo.castillo@global3.cl>
2022-10-03 11:20:03 -0300

    Feat: Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-  mode: 'jit', // Just-In-Time Compiler
-  purge: ['./src/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.html'],
+  class: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': 'Baumans, cursive',
+      'serif': 'Quicksand, sans-serif',
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'plannet-log': "url(/img/plannetlog_bg.jpg)",
+        'plannet-welcome': "url(/img/welcome-section.jpg)",
+        'plannet-login-mob': "url(/img/bg-login-mobile.png)"
+      }),
+      colors: {
+        'plannet-green': '#3cb573',
+        'plannet-green-dark': '#309778',
+        'plannet-purple': '#52053e',
+        'plannet-purple-dark': '#1e0335',
+      }
+    },
   },
   variants: {
     extend: {},
