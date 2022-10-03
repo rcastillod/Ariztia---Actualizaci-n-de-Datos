c36e4e6a83b0adcd61f2853ba0802072acafaa0f
rcastillod <rodrigo.castillo@global3.cl>
2022-10-03 16:06:29 -0300

    Feat: Validatino form

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,81 @@
 /** Form Validation */
-const btn = document.querySelector('.btn');
-const btnInput = document.querySelector('input[type=submit]');
-const spinner = document.querySelector('.loading');
-const submit = document.getElementById('submit');
-const alerta = document.getElementById("alerta");
-const mensaje = document.getElementById("mensaje");
+const form = document.getElementById('form-log')
 
-/*submit.addEventListener('click', () => {
-    btn.classList.add('submit');
-    btnInput.value = "";
-    spinner.classList.add('show');
-})*/
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-// Permitir sólo números en el imput
-function isNumber(evt) {
-  var charCode = evt.which ? evt.which : event.keyCode;
-  if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode === 75) return false;
+  limpiaErrores()
 
-  return true;
-}
-
-function checkRut(rut) {
-
-  if (rut.value.length <= 1) {
-    mensaje.classList.remove('text-plannet-green', 'text-red-500');
-    mensaje.classList.add('text-blue-500');
-    mensaje.innerHTML = 'Ingrese un RUT en el siguiente campo de texto para validar si es correcto o no';
-  }
-
-  // Obtiene el valor ingresado quitando puntos y guión.
-  var valor = clean(rut.value);
-
-  // Divide el valor ingresado en dígito verificador y resto del RUT.
-  cuerpo = valor.slice(0, -1);
-  dv = valor.slice(-1).toUpperCase();
-
-  // Separa con un Guión el cuerpo del dígito verificador.
-  rut.value = format(rut.value);
-
-  // Si no cumple con el mínimo ej. (n.nnn.nnn)
-  if (cuerpo.length < 7) {
-    rut.setCustomValidity("RUT Incompleto");
-    mensaje.classList.remove('text-plannet-green', 'text-red-500');
-    mensaje.classList.add('text-blue-500');
-    mensaje.innerHTML = 'Ingresó un RUT muy corto, el RUT debe ser mayor a 7 Dígitos. Ej: 1.111.111-1';
-    return false;
-  }
-
-  // Calcular Dígito Verificador "Método del Módulo 11"
-  suma = 0;
-  multiplo = 2;
-
-  // Para cada dígito del Cuerpo
-  for (i = 1; i <= cuerpo.length; i++) {
-    // Obtener su Producto con el Múltiplo Correspondiente
-    index = multiplo * valor.charAt(cuerpo.length - i);
-
-    // Sumar al Contador General
-    suma = suma + index;
-
-    // Consolidar Múltiplo dentro del rango [2,7]
-    if (multiplo < 7) {
-      multiplo = multiplo + 1;
-    } else {
-      multiplo = 2;
-    }
-  }
-
-  // Calcular Dígito Verificador en base al Módulo 11
-  dvEsperado = 11 - (suma % 11);
-
-  // Casos Especiales (0 y K)
-  dv = dv == "K" ? 10 : dv;
-  dv = dv == 0 ? 11 : dv;
-
-  // Validar que el Cuerpo coincide con su Dígito Verificador
-  if (dvEsperado != dv) {
-    rut.setCustomValidity("RUT Inválido");
-
-    mensaje.classList.remove('text-blue-500', 'text-plannet-green');
-    mensaje.classList.add('text-red-500');
-    mensaje.innerHTML = 'El RUT ingresado: ' + rut.value + ' Es <strong>INCORRECTO</strong>.';
-
-    return false;
+  if (validaCampos()) {
+    mensajeExito()
   } else {
-    rut.setCustomValidity("RUT Válido");
-
-    mensaje.classList.remove('hidden', 'text-red-500');
-    mensaje.classList.add('text-plannet-green');
-    mensaje.innerHTML = 'El RUT ingresado: ' + rut.value + ' Es <strong>CORRECTO</strong>.';
-    return true;
-  }
-}
-
-function format (rut) {
-  rut = clean(rut)
-
-  var result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
-  for (var i = 4; i < rut.length; i += 3) {
-    result = rut.slice(-3 - i, -i) + '.' + result
+    mensajeError()
   }
 
-  return result
+})
+
+const validaCampos = () => {
+  let telefono = document.getElementById('telefono').value
+  let email = document.getElementById('email').value
+  let valid = true
+
+  if (telefono == '') {
+    valid = false
+    document.querySelector('.errorTelefono').innerHTML = 'El campo es obligatorio'
+  } else if (validaTelefono(telefono) == false) {
+    valid = false
+    document.querySelector('.errorTelefono').innerHTML = 'El formato del telefono es incorrecto'
+  }
+
+  if (email == '') {
+    valid = false
+    document.querySelector('.errorEmail').innerHTML = 'El campo es obligatorio'
+  } else if (validaEmail(email) == false) {
+    valid = false
+    document.querySelector('.errorEmail').innerHTML = 'El formato del correo es incorrecto'
+
+  }
+
+  return valid
+
 }
 
-function clean (rut) {
-  return typeof rut === 'string'
-    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
-    : ''
+const validaTelefono = (telefono) => {
+  let pattern = /^(\+?56)?(\s?)(0?9)(\s?)[98765432]\d{7}$/
+  let textResult = pattern.test(telefono)
+  return textResult
+}
+
+const validaEmail = (email) => {
+  let pattern = /.+@.+\..+/
+  let textResult = pattern.test(email)
+  return textResult
+}
+
+const limpiaErrores = () => {
+  document.querySelector('.errorTelefono').innerHTML = ''
+  document.querySelector('.errorEmail').innerHTML = ''
+}
+
+const limpiaCampos = () => {
+  let telefono = document.getElementById('telefono').value = ''
+  let email = document.getElementById('email').value = ''
+}
+
+const mensajeExito = () => {
+  let msg = document.getElementById('message')
+  msg.innerHTML = 'Tus datos han sido actualizados con éxito!'
+  msg.classList.remove('error')
+  msg.classList.add('show', 'exito')
+  setTimeout(() => {
+    msg.classList.remove('show')
+  }, 2000);
+  limpiaCampos()
+}
+
+const mensajeError = () => {
+  let msg = document.getElementById('message')
+  msg.innerHTML = 'Uno de los campos tiene error.'
+  msg.classList.remove('exito')
+  msg.classList.add('show', 'error')
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,16 @@ module.exports = {
     fontFamily: {
       'sans': 'Baumans, cursive',
       'serif': 'Quicksand, sans-serif',
+      'century': 'Century Gothic'
     },
     extend: {
       backgroundImage: theme => ({
-        'plannet-log': "url(/img/plannetlog_bg.jpg)",
-        'plannet-welcome': "url(/img/welcome-section.jpg)",
-        'plannet-login-mob': "url(/img/bg-login-mobile.png)"
+        'ariztia-log': "url(/img/ariztialog-bg.jpeg)",
+        'ariztia-welcome': "url(/img/welcome-section.jpg)",
       }),
       colors: {
-        'plannet-green': '#3cb573',
-        'plannet-green-dark': '#309778',
-        'plannet-purple': '#52053e',
-        'plannet-purple-dark': '#1e0335',
+        'ariztia-red': '#FF0000',
+        'ariztia-red-dark': '#DC0B03',
       }
     },
   },
